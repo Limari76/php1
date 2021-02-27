60ad7fd1a05b9a6fa2273b6ed33d2020392a751e
Limari76 <73397504+Limari76@users.noreply.github.com>
2021-02-27 13:54:16 -0600

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mi Web</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="js/fontawesome6.js"></script>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Architects+Daughter&family=Kalam:wght@300&family=Permanent+Marker&display=swap" rel="stylesheet">
+</head>
+<body>
+	
+<!-- Formulario -->
+
+	<div class="sec1  col-l-12 col-m-12 col-m-12">
+		<div class="contenedor">
+			<div class="col-l-6 col-m-6 desaparecermovil">
+				<div class="imagenslider">
+					<img src="img/img1.png">
+				</div>
+			</div>
+			<div class="col-l-6 col-m-6 ">
+				<div class="formulario">
+					<h2> Para mas informacion</h2>
+					<form method="post" action="">
+						<input type="text" name="" placeholder="Nombre">
+						<input type="text" name="" placeholder="Apellido">
+						<input type="email" name="" placeholder="Correo">
+						<button type="submit">Enviar</button>
+					</form>
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+<!-- Ventajas-->
+	<div class="sec2  col-l-12 col-m-12 col-s-12">
+		<div class="contenedor">
+			<div class="col-l-12 col-m-12 col-m-12">
+				<div class="titulos">
+					<h3>Ventajas</h3>
+				</div>
+				
+			</div>
+			<div class="col-l-4 col-m-4 col-s-12">
+				<div class="contenidoventaja">
+					<div class="tituloventaja">
+						<div class="col-m-2 col-l-2 col-s-2">
+							<div class="icono">
+							<i class="fas fa-umbrella-beach"></i>	
+							</div>
+							
+							
+						</div>
+						<div class="col-m-10 col-l-10">
+							<div class="subtitulo">
+								<span>Titulo 1</span>	
+							</div>
+						</div>
+
+						
+					</div>
+					<div class="col-m-12">
+						<div class="parrafoventaja">
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+					</div>
+
+				</div>
+				
+			</div>
+			<div class="col-l-4 col-m-4 col-s-12">
+				<div class="contenidoventaja">
+					<div class="tituloventaja">
+						<div class="col-m-12 col-l-12 col-s-12">
+							<div class="icono2">
+								<i class="fas fa-water"></i>
+								<span>Titulo 2</span>	
+							</div>
+						</div>
+					</div>
+					<div class="col-l-12">
+						<div class="parrafoventaja">
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+
+					</div>
+
+				</div>
+				
+			</div>
+			<div class="col-l-4 col-m-4 col-s-12">
+				<div class="contenidoventaja">
+					<div class="tituloventaja">
+						<div class="col-m-12 col-l-12 col-s-12">
+							<div class="icono3">
+								<span>Titulo 3</span>	
+								<i class="far fa-sun"></i>
+							</div>
+						</div>
+					</div>
+					<div class="col-l-12">
+						<div class="parrafoventaja">
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+							</p>
+						</div>
+
+					</div>
+
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+
+<!-- Caracteristicas -->
+	<div class="sec3  col-l-12 col-m-12 col-m-12">
+		<div class="contenedor">
+			<div class="col-l-12 col-m-12 col-m-12">
+				<div class="titulos">
+					<span>Caracteristicas</span>
+				</div>
+				
+			</div>
+			<div class="col-l-6 col-m-6 desaparecermovil">
+				<div class="imagenslider2">
+					<img src="img/img2.png">
+				</div>
+			</div>
+			<div class="col-l-6 col-m-6">
+				<div class="contenidocaracteristicas">
+					<div class="col-m-12 col-l-12 col-s-12">
+						<div class="icono4">
+							<i class="fas fa-sun"></i>
+							<span>Caracteristica 1</span>	
+						</div>
+					</div>
+				
+					<div class="col-l-12">
+						<div class="parrafocaracteristicas">
+							<p>
+							Tarifas competitivas
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="contenidocaracteristicas">
+					<div class="col-m-12 col-l-12 col-s-12">
+						<div class="icono4">
+							<i class="fas fa-sun"></i>
+							<span>Caracteristica 2</span>	
+						</div>
+					</div>
+				
+					<div class="col-l-12">
+						<div class="parrafocaracteristicas">
+							<p>
+							Facilidades de primera
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="contenidocaracteristicas">
+					<div class="col-m-12 col-l-12 col-s-12">
+						<div class="icono4">
+							<i class="fas fa-sun"></i>
+							<span>Caracteristica 3</span>	
+						</div>
+					</div>
+				
+					<div class="col-l-12">
+						<div class="parrafocaracteristicas">
+							<p>
+							Menu a la carte y buffet
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="contenidocaracteristicas">
+					<div class="col-m-12 col-l-12 col-s-12">
+						<div class="icono4">
+							<i class="fas fa-sun"></i>
+							<span>Caracteristica 4</span>	
+						</div>
+					</div>
+				
+					<div class="col-l-12">
+						<div class="parrafocaracteristicas">
+							<p>
+							Habitaciones comodas y desinfectadas diariamente
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- Parallax -->
+<!-- inicia parallax-->
+<div class="col-l-12">
+	<section class="parallax" id="seccion1">
+		<div class="text-parallax">
+			<h2>Mi Seccion Parallax</h2>
+		</div>
+	</section>	
+</div>
+
+<!-- Galeria-->
+	<section id="bg-lavender">
+		
+	</section>
+
+<!--Footer-->
+	
+</body>
+</html>
---
+++ web/interfaz/footer_4.php
@@ -0,0 +1,62 @@
+<div class="footer col-l-12 col-m-12 col-s-12">
+		<div class="contenedor">
+			<div class="col-m-12">
+				<div class="col-l-3 col-m-6 col-s-12">
+					<div class="sec-footer">
+						<div class="logo">
+							<img src="img/logo.png">
+						</div>
+						<span>Las mejores y más seguras vacaciones de tu vida. Te garantizamos una experiencia que llevarás contigo en el corazón</span>	
+					</div>
+				</div>
+				<div class="col-l-3 col-m-6 col-s-12">
+					<div class="sec-footer">
+						<div class="contacto">
+							<span>Contáctanos</span>
+						</div>
+						<div class="col-l-12">
+							<div class="icono5">
+								<i class="far fa-envelope"></i>
+								<span>playita@gmail.com</span>
+							</div>
+							<div class="icono5">
+								<i class="fas fa-mobile-alt"></i>
+								<span>(33)1234-5678</span>
+							</div>
+							<div class="icono5">
+								<i class="fas fa-map-marker-alt"></i>
+								<span>Ave. Arena #1234</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-m-12">
+				<div class="col-l-3 col-m-6 col-s-12">
+					<div class="sec-footer">
+						<div class="contacto">
+							<span>Links</span>
+						</div>
+						<div class="col-l-12">
+							<div class="links">
+								<a href="url">playita.com</a> <br>
+								<a href="url">Trip Advisor</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-l-3 col-m-6 col-s-12">
+					<div class="sec-footer">
+						<div class="">
+							<div class="contacto">
+								<span>Ubícanos</span>
+							</div>
+							<div class="mapa">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14932.547751109576!2d-105.26429226457611!3d20.664007129152296!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2smx!4v1614446837198!5m2!1ses-419!2smx" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
